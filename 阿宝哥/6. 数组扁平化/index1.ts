39fbf65c47859662838f8292882f983e79dbcb0d
lihaohao <2495041749@qq.com>
2021-12-01 18:15:33 +0800

    feat: 数组扁平å化

--- 阿宝哥/6. 数组扁平化/index1.ts
+++ 阿宝哥/6. 数组扁平化/index1.ts
@@ -1,9 +1,9 @@
 type NaiveFlat<T extends any[]> = {
-  [P in keyof T]: T[P] extends any[] ? T[P][number] : T[P]
+  [P in keyof T]: T[P] extends any[] ? NaiveFlat<T[P]> : T[P]
 }[number]
 
 // 测试用例：
-  type NaiveResult = NaiveFlat<[['a'], ['b', 'c'], ['d']]>
+  type NaiveResult = NaiveFlat<[['a'], ['b', ['c']], ['d']]>
 // NaiveResult的结果： "a" | "b" | "c" | "d"
 
 export default {}
